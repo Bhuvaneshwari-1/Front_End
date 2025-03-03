0080c0c398ba2d0f75539ce9caccfa45a0f6a5b9
Bhuvaneshwari-1 <buvaneshwari393@gmail.com>
2025-03-03 17:31:33 +0530

    Initial commit

---
+++ Typescript/mathOperations.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+//Q10 - Exporting Function
+// mathOperations.ts
+function add(a, b) {
+    return a + b;
+}
---
+++ Typescript/mathOperations.ts
@@ -0,0 +1,5 @@
+//Q10 - Exporting Function
+// mathOperations.ts
+export function add(a: number, b: number): number {
+    return a + b;
+}
